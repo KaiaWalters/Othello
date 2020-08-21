71f65f9e86d0f74a22bb58b04720141fa2c2bddc
StefanPowell <stefanpowell100@icloud.com>
2020-08-21 01:28:15 -0400

    User inputs and game logging

--- js/start.js
+++ js/start.js
@@ -1,17 +1,5 @@
-cols = ["a", "b", "c", "d", "e", "f", "g", "h"];
-rows = [1, 2, 3, 4, 5, 6, 7, 8];
-
-//hide all elements
-function hide_elements(cols, rows){
-    for(i=0; i < cols.length; i++){
-      for(j=0; j < rows.length; j++){
-        element_id = (cols[i].concat(rows[j].toString()));
-        document.getElementById(element_id).style.display = "none";
-      }
-    }
-}
-
 //set starting four - two white [d4, e5] and two black [d5, e4]
+
 function begin(){
   var startingvalues = {};
   startingvalues["d4"] = "#FFFFFF";
@@ -26,7 +14,38 @@ function begin(){
   }
 }
 
+function disableinputboxes(){
+  var player1 = document.getElementById("first_player_input").value;
+  var player2 = document.getElementById("second_player_input").value;
+  if( player1 == "" || player2 == ""){
+    return [];
+  }else{
+    return [player1, player2];
+  }
+}
+
+function log_data(text){
+  current_logs = document.getElementById("logs").innerHTML;
+  new_logs = text.concat(current_logs);
+  document.getElementById("logs").innerHTML = new_logs;
+}
+
 function start_game(){
-  hide_elements(cols, rows);
   begin();
+  var players = disableinputboxes();
+  if(players.length < 2){
+    //log tell them to add players names
+    log_data("<div> > Player names cant be null</div>");
+  }else{
+    //disable textboxes
+    log_data("<div> > Start Game </div>");
+    document.getElementById("first_player_input").disabled = true;
+    log_data("<div> > Player 1 : " + players[0] + "</div>");
+    document.getElementById("second_player_input").disabled = true;
+    log_data("<div> > Player 2 : " + players[1] + "</div>");
+    document.getElementById("main_btn").disabled = true;
+    document.getElementById("main_btn").style.backgroundColor = "transparent";
+    document.getElementById("main_btn").style.color = "#000000";
+    document.getElementById("main_btn").innerHTML = players[0] + " <span id='player1_score'>0</span>  -- <span id='player2_score'>0</span> " + players[1];
+  }
 }
